f12f8572c26fd9a5bd74732403485b6e973db64a
Austin Phy <atphy42@gmail.com>
2020-10-08 09:42:17 -0500

    all get requests completed

---
+++ locations/request.py
@@ -0,0 +1,35 @@
+LOCATIONS = [
+    {
+        "id": 1,
+        "name": "location1",
+        "species": "location1",
+        "locationId": 1,
+        "customerId": 4
+    },
+    {
+        "id": 2,
+        "name": "location2",
+        "species": "location2",
+        "locationId": 1,
+        "customerId": 2
+    },
+    {
+        "id": 3,
+        "name": "location3",
+        "species": "location3",
+        "locationId": 2,
+        "customerId": 1
+    }
+]
+
+def get_single_location(id):
+    requested_location = None
+
+    for location in LOCATIONS:
+        if location["id"] == id:
+            requested_location = location
+    
+    return requested_location
+
+def get_all_locations():
+    return LOCATIONS
